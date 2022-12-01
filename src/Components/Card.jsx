62ba767311db134c86cf47f520184bb63d6fd172
ArchSofia <sofiadiazvaldez@gmail.com>
2022-12-01 18:00:39 -0300

    primer commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Card = ({ name, username, id }) => {
+  const 
+	const addFav = () => {
+		// Aqui iria la logica para agregar la Card en el localStorage
+	};
 
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
+	return (
+		<>
+			{odontologos.map((odontologos) => (
+				<div className="card" key={odontologos.id}>
+					<h2>odontologos.name</h2>
+					<h3>odontologos.username</h3>
+					<h3>odontologos.id</h3>
+					<Link to="/detail">Favs </Link>
 
-  return (
-    <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+					{/* En cada card deberan mostrar en name - username y el id */}
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+					{/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
-    </div>
-  );
+					{/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+					<button onClick={addFav} className="favButton">
+						Add fav
+					</button>
+				</div>
+			))}
+		</>
+	);
 };
 
 export default Card;
