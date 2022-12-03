a708e564e5d5649d35fa4197fa5e83eaab835ccb
uancamilo <uancamilo@gmail.com>
2022-12-03 00:44:46 -0500

    Fetch tarjetas y tailwindcss

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,53 @@
 import React from "react";
-
+import { useEffect } from "react";
 
 const Card = ({ name, username, id }) => {
+	const [odontologos, setOdontologos] = React.useState([]);
+	useEffect(() => {
+		async function getOdontologo() {
+			try {
+				const response = await fetch(
+					"https://jsonplaceholder.typicode.com/users"
+				);
+				const json = await response.json();
+				setOdontologos(
+					json.map((odontologo) => {
+						return {
+							id: odontologo.id,
+							name: odontologo.name,
+							username: odontologo.username,
+						};
+					})
+				);
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		getOdontologo();
+	}, []);
 
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
+	const addFav = () => {
+		// Aqui iria la logica para agregar la Card en el localStorage
+	};
 
-  return (
-    <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+	const imgStyle = { height: "60px", width: "60px" };
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+	return (
+		<>
+			{odontologos.map((odontologo) => (
+				<div className="grid-cols-3" key={odontologo.id}>
+					<img src="./images/doctor.jpg" alt="Odontologo" style={imgStyle} />
 
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
-    </div>
-  );
+					<button onClick={addFav} className="favButton">
+						Add fav
+					</button>
+				</div>
+			))}
+			{/* En cada card deberan mostrar en name - username y el id */}
+			{/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+			{/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+		</>
+	);
 };
 
 export default Card;
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
-
-
