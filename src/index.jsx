1e169c73b14677a64b4b00f3317f297f8e36e78d
Gabriel Marquez <marquez122@outlook.com>
2023-04-04 08:48:43 -0500

    adds router

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./error-page";
+import Home from "./Routes/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "home",
+                element: <Home />,
+            },
+            {
+                path: "dentist/:id",
+                element: <Dentist />,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+            },
+            {
+                path: "favs",
+                element: <Favs />
+            }
+        ]
+    },
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App/>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
