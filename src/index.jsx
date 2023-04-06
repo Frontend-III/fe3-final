af9ea5777f32afb45190a14cdce96d42db63410d
marquez5g <marquez122@outlook.com>
2023-04-06 17:51:01 -0500

    Updates main element

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from "./App";
 import ErrorPage from "./error-page";
 import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Dentist from "./Routes/Dentist";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Root />,
+        element: <App />,
         errorElement: <ErrorPage />,
         children: [
             {
-                path: "home",
+                path: "/",
                 element: <Home />,
             },
             {
