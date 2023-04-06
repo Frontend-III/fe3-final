bf6fbd73756838d3ece377d11909937c6284eb8e
marquez5g <marquez122@outlook.com>
2023-04-06 17:50:23 -0500

    Uses global context to load dentists

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import Card from '../Components/Card'
+import {ContextGlobal} from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
 const Home = () => {
-  return (
+    const { state } = useContext(ContextGlobal);
+
+    return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
+        {state.dentists && state.dentists.map(d => <Card key={d.id} name={d.name} username={d.username} id={d.id}/>)}
       </div>
     </main>
   )
