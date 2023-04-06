827f50085ade46205acec857f80c015bb774cc57
marquez5g <marquez122@outlook.com>
2023-04-06 17:48:41 -0500

    Adds logic to clear favorites and to render them with a reducer from localStorage

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 import Card from "../Components/Card";
+import {ContextGlobal} from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
-
+    const { state, dispatch } = useContext(ContextGlobal);
+    const clearFavorites = () => dispatch({type: "CLEAR_FAVORITES"});
   return (
     <>
       <h1>Dentists Favs</h1>
+        <button onClick={clearFavorites}>Limpiar</button>
       <div className="card-grid">
+          {state.favs && state.favs.map(d => <Card key={d.id} name={d.name} username={d.username} id={d.id}/>)}
         {/* este componente debe consumir los destacados del localStorage */}
         {/* Deberan renderizar una Card por cada uno de ellos */}
       </div>
