bdfa74680a1204dedf2fa26374308368f8e10fdc
marquez5g <marquez122@outlook.com>
2023-04-06 17:49:52 -0500

    global context with reducer to handle api call, favorites and localStorage

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,92 @@
-import { createContext } from "react";
+import {createContext, useMemo, useReducer} from "react";
 
-export const initialState = {theme: "", data: []}
+const loadDentists = () => {
+    const localDentists = localStorage.getItem("dentists");
+    if (!localDentists) {
+        const getData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const parsedResponse = await response.json();
+            localStorage.setItem("dentists", JSON.stringify(parsedResponse));
+            console.log("API called!");
+            return parsedResponse;
+        }
+        return getData();
+    } else {
+        console.log("local dentists were found!");
+        return JSON.parse(localDentists);
+    }
+}
 
-export const ContextGlobal = createContext(undefined);
+const loadFavoriteDentists = () => {
+    return getFavoriteDentists() ;
+}
 
-export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+function getFavoriteDentists() {
+    return JSON.parse(localStorage.getItem("favorite-dentists")) || [];
+}
 
-  return (
-    <ContextGlobal.Provider value={{}}>
-      {children}
-    </ContextGlobal.Provider>
-  );
+export const isFavorite = (id) => {
+    let favoriteDentists = getFavoriteDentists();
+    return favoriteDentists.some(dentist => dentist.id === id);
+}
+
+const addFavoriteDentist = (d) => {
+    let favoriteDentists = getFavoriteDentists();
+    if(isFavorite(d.id)) {
+        console.log("dentists already a favorite");
+        return;
+    }
+    favoriteDentists.push({name: d.name, username: d.username, id: d.id, email: d.email, phone: d.phone, website: d.website})
+    localStorage.setItem("favorite-dentists", JSON.stringify(favoriteDentists));
+    return favoriteDentists;
 };
+const removeFavoriteDentist = (id) => {
+    //check if its fav already
+    let favoriteDentists = getFavoriteDentists();
+    let index = favoriteDentists.findIndex(dentist => dentist.id = id)
+    favoriteDentists.splice(index, 1);
+    localStorage.setItem("favorite-dentists", JSON.stringify(favoriteDentists));
+    return favoriteDentists;
+};
+const clearFavorites = () => {
+    localStorage.removeItem("favorite-dentists");
+    return [];
+};
+export const reducer = async (state, action) => {
+    switch (action.type) {
+        case "SWITCH_THEME":
+            return {
+                ...state,
+                theme: state.theme === 'light' ? 'dark' : 'light'
+            };
+        case "ADD_FAVORITE":
+            return {
+                ...state,
+                favorites: addFavoriteDentist(action.payload)
+            };
+        case "REMOVE_FAVORITE":
+            return {
+                ...state,
+                favorites: removeFavoriteDentist(action.payload)
+            }
+        case "CLEAR_FAVORITES":
+            return {
+                ...state,
+                favorites: clearFavorites()
+            }
+        default:
+            return state;
+    }
+};
+
+const data = loadDentists();
+export const initialState = {
+    theme: "dark",
+    dentists: data,
+    favs: loadFavoriteDentists()
+}
+
+export const ContextGlobal = createContext();
+
+
+
