2614e6346722ad02fae92993dd1b0e3f163072c1
marquez5g <marquez122@outlook.com>
2023-04-06 19:08:58 -0500

    adds css to show when a dentist is fav

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext} from 'react'
 import Card from '../Components/Card'
 import {ContextGlobal} from "../Components/utils/global.context";
 
