f6e8c5190087933e1757bd1004520151b3261767
gabrielolivos <100105376+gabrielolivos@users.noreply.github.com>
2023-06-29 21:37:36 -0300

    Update Favs.jsx

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,18 +1,36 @@
-import React from "react";
-import Card from "../Components/Card";
+import CardFav from "../Components/CardFav";
+
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
 
-  return (
+
+  const favsLocal = JSON.parse(localStorage.getItem('favs'))
+
+  const existenFavs = () => {
+
+    if(favsLocal==null){
+      return false}
+    else if (favsLocal[0].did == null){  
+      favsLocal.shift()
+      return true}
+    else  return true}
+  
+  
+return (
+  existenFavs() ?
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
+      {favsLocal.map(dentist => <CardFav did={dentist.did} dname={dentist.dname} dusername={dentist.dusername}/>)}
         {/* este componente debe consumir los destacados del localStorage */}
         {/* Deberan renderizar una Card por cada uno de ellos */}
       </div>
     </>
+    :
+    <h4>Aun no existen favoritos</h4>
   );
 };
 
